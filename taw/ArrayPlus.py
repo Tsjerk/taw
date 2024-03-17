bf62740048f5cbc8021623b2081849894b75d074
tsjerk <tsjerkw@gmail.com>
2024-03-17 22:06:14 +0100

    Added class CoordinatesMaybeWithPBC

--- taw/ArrayPlus.py
+++ taw/ArrayPlus.py
@@ -85,4 +85,146 @@ class PBC(NDArrayWithAttributes):
         ...
 
 
+class CoordinatesMaybeWithPBC(NDArrayWithAttributes):
+    '''
+    The CoordinatesMaybeWithPBC class is intended to 
+    store trajectory data that may be accompanied by 
+    the periodic boundary condition (PBC) information. 
+    If present, the PBC must share the dimensions of
+    the trajectory data, except for the second last,
+    which is the number of coordinates in the trajectory
+    and the dimensionality in the PBC.
+    
+    Transformations that scale or rotate the coordinates
+    are also applied to the PBC. Translations are only
+    applied to coordinates.
+    
+    The class implements several properties for useful
+    transformations, namely to box coordinates and to
+    angles. Both of these are reversible.
+    
+    The class implements several operations on the 
+    coordinates using the PBC, including putting all
+    coordinates inside the base unit cell at the origin
+    or around the origin, and putting all coordinates
+    in a compact representation.
+    '''
+    _attributes = (
+        'pbc' # [..., 3, 3] lattice matrices
+    )
+    
+    def __new__(cls, *args, pbc=None, **kwargs):
+        coordinates = np.array(*args, **kwargs).view(cls)
+        coordinates.pbc = pbc
+        return coordinates
+        
+    def _pbcfunc(self, other, operation):
+        out = getattr(super(), operation)(other)
+        out.__array_finalize__(self)
+        if self.pbc is not None:
+            out.pbc = getattr(self.pbc, operation)(other)
+        return out
+        
+    def _pbcifunc(self, other, operation):
+        getattr(super(), operation)(other)
+        if self.pbc is not None:
+            getattr(self.pbc, operation)(other)
+        return self
+        
+    def __matmul__(self, other):
+        return self._pbcfunc(other, '__matmul__')
+    
+    def __mul__(self, other):
+        return self._pbcfunc(other, '__mul__')
+    
+    def __div__(self, other):
+        return self._pbcfunc(other, '__div__')
+    
+    def __imul__(self, other):
+        return self._pbcifunc(other, '__imul__')
+    
+    def __idiv__(self, other):
+        return self._pbcifunc(other, '__imul__')
+    
+    @property
+    def X(self):
+        '''Return bare coordinates'''
+        return self.view(np.ndarray)
+    
+    @property
+    def I(self):
+        '''Return box coordinates and inverse of lattice'''
+        if self.pbc is None:
+            return None
+        inv = np.linalg.inv(self.pbc)
+        # This sets the pbc to identity
+        out = self @ inv
+        #out.__array_finalize__(self)
+        # We reset the pbc with the inverse to allow
+        # the reverse operation.
+        out.pbc = inv
+        return out
+    
+    @property
+    def A(self):
+        '''Return coordinates as box angles'''
+        if self.pbc is None:
+            return None
+        inv = np.linalg.inv(self.pbc) * (2 * np.pi)
+        # This sets the pbc to 2\pi \times identity
+        out = self @ inv
+        #out.__array_finalize__(self)
+        # We reset the pbc with the inverse to allow
+        # the reverse operation with the .C property
+        # (It will work with .I, but that does not set
+        # the pbc correct afterwards)
+        out.pbc = inv
+        return out
+    
+    @property
+    def C(self):
+        '''Return Cartesian coordinates from box angles'''
+        if self.pbc is None:
+            return None
+        # self.pbc is 2\pi L^-1, inverse is L / 2\pi 
+        out = self.I
+        out.pbc *= (2 * np.pi)
+        return out
+        
+    def inbox(self):
+        '''Put all particles in triclinic unit cell'''
+        if self.pbc is None:
+            return None
+        B = self.I
+        B -= np.floor(B)
+        return B.I
+    
+    def originbox(self):
+        '''Put all particles in triclinic unit cell around the origin'''
+        B = self.I
+        B -= np.floor(B + 0.5)
+        return B.I
+    
+    def hexagonal(self):
+        '''
+        This routine puts the atoms in a compact,  
+        hexagonal representation around the origin.
+        '''
+        B = self.I.reshape((-1, 3))
+        # Shift origin to middle of box
+        B += 0.5
+        B -= np.floor(B)
+        x, y = B[:, :2].X.T
+        check = (y < 0.5 - x) | (y > 1.5 - x)
+        up = y > x
+        halfx = 0.5 * x
+        twox = 2 * x
+        B[check &  up & (y > 1.25 - halfx), 1] -= 1
+        B[check & ~up & (y < 0.25 - halfx), 1] += 1
+        B[check &  up & (y < 0.5 - twox), 0] += 1
+        B[check & ~up & (y > 2.5 - twox), 0] -= 1
+        # Shift middle of hexagon to origin and transform
+        B -= 0.5
+        return B.reshape(self.shape).I
+    
 
