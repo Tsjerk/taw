8c6f6221e4c078f06d274aaa1cdff3efc5b8e30d
Tsjerk <tsjerkw@gmail.com>
2024-12-18 16:16:06 +0100

    Added PBC related functionality to Trajectory

--- taw/Trajectory.py
+++ taw/Trajectory.py
@@ -132,70 +132,6 @@ class PBC(VectorArray):
         ...
 
 
-class old_PBC(NDArrayWithAttributes):
-    '''
-    The PBC class contains an ndarray of which the last two axes
-    have shape k by k, and contain a k-dimensional lattice. The
-    PBC class may contain its own inverse and implements 
-    properties to give access to the lattice, the inverse and
-    the transformation to angles. The latter will always contain
-    their own inverses for the reverse transformation.
-    '''
-    
-    _attributes = (
-        'inverse'
-    )
-    
-    def __new__(cls, pbc, inverse=None):
-        if pbc.shape[-1] == 6:
-            pbc = cls.dim2pbc(pbc)
-        pbc = pbc.view(cls)
-        pbc._inverse = inverse
-        return pbc
-        
-    @classmethod
-    def dim2pbc(self, arr: np.ndarray) -> np.ndarray:
-        '''
-        Convert unit cell definition from PDB CRYST1 format to lattice definition.
-        '''
-        lengths = arr[..., :3].reshape((-1, 3))
-        angles = arr[..., 3:].reshape((-1, 3)) * (np.pi / 180)
-
-        cosa = np.cos(angles)
-        sing = np.sin(angles[:, 2])
-
-        pbc = np.zeros((len(arr), 9))
-        pbc[:, 0] = lengths[:, 0]
-        pbc[:, 3] = lengths[:, 1] * cosa[:, 2]
-        pbc[:, 4] = lengths[:, 1] * sing
-        pbc[:, 6] = lengths[:, 2] * cosa[:, 1]
-        pbc[:, 7] = lengths[:, 2] * (cosa[:, 0] - cosa[:, 1] * cosa[:, 2]) / sing
-        pbc[:, 8] = (lengths[:, 2] ** 2 - (pbc[:, 6:8] ** 2).sum(axis=1)) ** 0.5
-
-        return pbc.reshape((*arr.shape[:-1], 3, 3))
-    
-    @property
-    def L(self):
-        '''Return the 'naked' lattice vectors'''
-        return self.view(np.ndarray)
-    
-    @property
-    def I(self):
-        '''Return inverse lattice (mapping to unit cube)'''
-        if self._inverse is None:
-            self._inverse = np.linalg.inv(self.L)
-        return PBC(self._inverse, self.L)
-    
-    @property
-    def A(self):
-        '''Return transformation to angles'''
-        return PBC(self.I * (2 * np.pi), self.L * (0.5 / np.pi))
-    
-    def reduce(self):
-        '''Perform lattice reduction'''
-        ...
-
-
 class CoordinatesMaybeWithPBC(NDArrayWithAttributes):
     '''
     The CoordinatesMaybeWithPBC class is intended to 
@@ -359,6 +295,14 @@ class Trajectory(MultiArray):
             argument = locals()[m]
             setattr(self, m, argument.view(t) if argument is not None else None)
 
+    def __repr__(self):
+        nf, na, nd = self.X.shape
+        b = (self.B is None) * 'not '
+        v = (self.V is None) * 'not '
+        f = (self.F is None) * 'not '
+        s = f'Trajectory with {nf} frames, {na} atoms and {nd} dimensions. PBC {b}present. Velocities {v}present. Forces {f}present.'
+        return s
+    
     @property
     def X(self):
         return self.coordinates
@@ -389,4 +333,48 @@ class Trajectory(MultiArray):
         setattr(out, 'pbc', inv)
         return out
        
+    def inbox(self):
+        '''Put all particles in triclinic unit cell'''
+        if self.pbc is None:
+            return None
+        B = self.I
+        B -= np.floor(B.X)
+        return B.I
+    
+    def originbox(self):
+        '''Put all particles in triclinic unit cell around the origin'''
+        B = self.I
+        B -= np.floor(B.X + 0.5)
+        return B.I
+    
+    def hexagonal(self):
+        '''
+        This routine puts the atoms in a compact,  
+        hexagonal representation around the origin.
+        '''
+        B = self.I # .reshape((-1, 3))
+        X = B.X
+        # Shift origin to middle of box
+        X += 0.5
+        X -= np.floor(X)
+        x, y = X[:, :2].T
+        check = (y < 0.5 - x) | (y > 1.5 - x)
+        up = y > x
+        halfx = 0.5 * x
+        twox = 2 * x
+        X[check &  up & (y > 1.25 - halfx), 1] -= 1
+        X[check & ~up & (y < 0.25 - halfx), 1] += 1
+        X[check &  up & (y < 0.5 - twox), 0] += 1
+        X[check & ~up & (y > 2.5 - twox), 0] -= 1
+        # Shift middle of hexagon to origin and transform
+        X -= 0.5
+        B.X = X.reshape(self.X.shape)
+        return B.I
+    
+    def dodecahedral(self):
+        # Easiest is to have a square base.
+        # If the base is hexagonal, rotate,
+        # do the stuff and rotate back.
+        # No one will notice.
+        ...
     
